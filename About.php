0511384414e9aa6a57a8b6f10efbaa712be98386
OthmanBouch <Ot.Bouchentouf@aui.ma>
2024-05-08 13:15:18 +0100

    favicon .fail

--- About.php
+++ About.php
@@ -1,7 +1,9 @@
 <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css" />
 <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@200&display=swap" rel="stylesheet" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <h1><span style="color:antiquewhite ;">Ifrane</span><span style="color:peru;">Warehouse</span></h1>
+
 <style>
 
 
